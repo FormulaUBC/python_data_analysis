fc90c51992e63ffeee4dd91aa77ddf70ff034f2d
cyrusneary <cyrusneary@gmail.com>
2016-11-24 22:31:56 -0800

    making changes to gg diagram

--- plots.py
+++ plots.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 from aim_data_csv_read import Data
+import numpy as np
 
 def plot_ax_vs_t(dat):
     fig = plt.figure()
@@ -9,5 +10,14 @@ def plot_ax_vs_t(dat):
 
 def g_g_diagram(dat):
     fig = plt.figure()
-    plt.plot(dat.data['AccelerometerX']['data'], dat.data['AccelerometerY']['data'], '+')
+    gx_temp = dat.data['AccelerometerX']['data']
+    gy_temp = dat.data['AccelerometerY']['data']
+    gx = []
+    gy = []
+    for i in range(len(gx_temp) - 1):
+        if np.sqrt(np.abs(float(gx_temp[i+1]) - float(gx_temp[i]))**2 + np.abs(float(gy_temp[i+1]) - float(gy_temp[i]))**2) < 2:
+            gx.append(gx_temp[i+1])
+            gy.append(gy_temp[i+1])
+
+    plt.plot(gx, gy, '+')
     plt.show()
